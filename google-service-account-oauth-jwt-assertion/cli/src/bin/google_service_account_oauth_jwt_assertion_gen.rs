5fc0842b64514f97ef704ce24bc082807566b4b7
vkill <vkill.net@gmail.com>
2023-03-13 22:09:13 +0800

    Tweak

--- google-service-account-oauth-jwt-assertion/cli/src/bin/google_service_account_oauth_jwt_assertion_gen.rs
+++ google-service-account-oauth-jwt-assertion/cli/src/bin/google_service_account_oauth_jwt_assertion_gen.rs
@@ -5,17 +5,21 @@ cargo install google-service-account-oauth-jwt-assertion-cli
 google_service_account_oauth_jwt_assertion_gen '/path/project-123456-123456789012.json' 'https://www.googleapis.com/auth/androidpublisher'
 */
 
-use std::{env, fs};
+use std::{env, fs, path::PathBuf};
 
 use google_service_account_oauth_jwt_assertion::create_from_service_account_json_key;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let service_account_json_key_path = env::args().nth(1).unwrap();
-    let scopes = env::args().nth(2).unwrap();
+    let service_account_json_key_path = env::args()
+        .nth(1)
+        .ok_or("args service_account_json_key_path missing")?
+        .parse::<PathBuf>()
+        .map_err(|_| "args service_account_json_key_path invalid")?;
+    let scopes = env::args().nth(2).ok_or("args scopes missing")?;
 
     let service_account_json_key_bytes = fs::read(service_account_json_key_path)?;
 
-    let client_secret = create_from_service_account_json_key(
+    let assertion = create_from_service_account_json_key(
         service_account_json_key_bytes,
         scopes
             .split(' ')
@@ -24,7 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .as_ref(),
     )?;
 
-    println!("{client_secret}");
+    println!("{assertion}");
 
     Ok(())
 }
